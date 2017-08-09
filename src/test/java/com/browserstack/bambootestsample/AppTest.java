031a73e87ec8dedec19f445ad4d1ea59698fe4d8
Danesh Qureshi <danesh.qureshi@gmail.com>
2017-08-09 19:02:29 +0530

    changes

--- src/test/java/com/browserstack/bambootestsample/AppTest.java
+++ src/test/java/com/browserstack/bambootestsample/AppTest.java
@@ -1,5 +1,12 @@
 package com.browserstack.bambootestsample;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -36,8 +43,16 @@ public class AppTest
         assertTrue( true );
     }
     
-    public void testDan()
+    public void testDan() throws MalformedURLException
     {
-        assertTrue( true );
+    	String username = System.getenv("BROWSERSTACK_USERNAME");
+    	String accessKey = System.getenv("BROWSERSTACK_ACCESS_KEY");
+
+    	DesiredCapabilities capabilities = new DesiredCapabilities();
+    	capabilities.setCapability("os", "Windows");
+    	capabilities.setCapability("browser", "chrome");
+    	WebDriver driver = new RemoteWebDriver(new URL("https://" + username + ":" + accessKey + "@hub.browserstack.com/wd/hub"), capabilities);
+    	driver.get("https://www.csgo500.com");
+    	assertTrue( true );
     }
 }
